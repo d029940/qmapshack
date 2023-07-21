98d83d3d9e94465f97e644293e07d07206ed7c5c
Manfred Kern <manfred.kern@gmail.com>
2023-07-21 21:33:33 +0200

    Do not delete qmapshack repo in clean script

--- MacOSX/clean.sh
+++ MacOSX/clean.sh
@@ -14,10 +14,9 @@ rm -rf $QMSDEVDIR/local
 rm -rf $QMSDEVDIR/quazip
 rm -rf $QMSDEVDIR/gdal
 rm -rf $QMSDEVDIR/routino*
-rm -rf $QMSDEVDIR/qmapshack 
 rm -rf $QMSDEVDIR/build_QMapShack
 
-echo "${ATTN}You can remove 1st_QMS_start.sh script from $QMSDEVDIR, if you don't want to build again${NC}"
+echo "${ATTN}You can remove the qmapshap git repo from $QMSDEVDIR by rm -rf $QMSDEVDIR/qmapshack ${NC}"
 
 
 
